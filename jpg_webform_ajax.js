61049db1839442847012e22d8a144d2abb39e7a2
forges82 <forges82@gmail.com>
2017-06-20 17:26:51 +0200

    functionality only if exists webform div

--- jpg_webform_ajax.js
+++ jpg_webform_ajax.js
@@ -2,7 +2,9 @@
   Drupal.behaviors.jpg_webform_ajax = {
     attach: function (context, settings) {
       $('.links a:not(.ajax-processed)', context).each(function () {
-	$(document).scrollTop( $("article.node-webform").offset().top );
+	if (($("article.node-webform").length > 0)){
+	  $(document).scrollTop( $("article.node-webform").offset().top );
+	}
     });
     }
   };
